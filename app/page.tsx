bc9171a74d28715cb2df89dd091984130abba443
cedricangulo <cdrcangulo@gmail.com>
2025-09-03 17:58:21 +0800

    feat: Add Laboratory 2 and Laboratory 3 content; remove Laboratory 1
    - Deleted Laboratory 1 file (m-lab1.mdx).
    - Added Laboratory 2 file (m-lab2.mdx) with form conversion task and code examples.
    - Added Laboratory 3 file (m-lab3.mdx) for assessment app development with multiple choice questions.
    - Introduced course sections data structure in course-sections.ts for better organization.

--- app/page.tsx
+++ app/page.tsx
@@ -1,75 +1,7 @@
 import { Plus } from "lucide-react"
 
 import CourseSection from "@/components/shared/course-section"
-
-const courseSections = [
-  {
-    subject: "IT-WST01",
-    year: "2023-2024 2nd sem",
-    teacher: "Rose Anne G. Cochanco",
-    section: "1E",
-    laboratoriesLink: "/itwst01",
-    caseStudyLink: "https://webwisee.vercel.app/",
-    className: "border-b md:border-r",
-  },
-  {
-    subject: "IT-WST02",
-    year: "2024-2025 1st sem",
-    teacher: "Rose Anne G. Cochanco",
-    section: "2A",
-    laboratoriesLink: "/itwst02",
-    caseStudyLink: "https://rnlsevilla.vercel.app/",
-    className: "border-b lg:border-r",
-  },
-  {
-    subject: "IT-PF01",
-    year: "2024-2025 1st sem",
-    teacher: "Prince Mert Nicolas",
-    section: "2A",
-    laboratoriesLink: "/itpf01",
-    className: "border-b md:border-r lg:border-r-0",
-  },
-  {
-    subject: "IT-PF02",
-    year: "2024-2025 2nd sem",
-    teacher: "Prince Mert Nicolas",
-    section: "2A",
-    laboratoriesLink: "/itpf02",
-    className: "border-b lg:border-b lg:border-r",
-  },
-  {
-    subject: "IT-HCI01",
-    year: "2024-2025 2nd sem",
-    teacher: "Marcelino Collado Jr.",
-    section: "2A",
-    laboratoriesLink: "/ithci01",
-    className: "md:border-r border-b md:border-b",
-  },
-  {
-    subject: "CC-104",
-    year: "2024-2025 2nd sem",
-    teacher: "Apple Oliveros",
-    section: "2A",
-    laboratoriesLink: "/cc104",
-    className: "border-b",
-  },
-  {
-    subject: "CC-105",
-    year: "2024-2025 2nd sem",
-    teacher: "Cris Norman Olipas",
-    section: "2A",
-    laboratoriesLink: "/cc105",
-    className: "border-b md:border-b-0 md:border-r",
-  },
-  {
-    subject: "IT-IPT01",
-    year: "2025-2026 1st sem",
-    teacher: "Melgine Bauat",
-    section: "3C",
-    laboratoriesLink: "/itipt01",
-    className: "lg:border-r",
-  },
-]
+import { courseSections } from "@/lib/course-sections"
 
 export default function Home() {
   return (
